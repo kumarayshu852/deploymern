c9261ea0912b737c13dba9e8379f904729b0e740
kumarayshu852 <kumarayshu852@gmail.com>
2025-12-17 16:13:20 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -26,7 +26,7 @@ function Signup(){
             return handleError('name, email and password are required');
         }
         try{
-            const url="http://localhost:8080/auth/signup";
+            const url="https://deploymern-api-pied.vercel.app/auth/signup";
             const response=await fetch(url,{
                 method: "POST",
                 headers:{

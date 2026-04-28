e3167166455d8583f299cb26d459e2a1c4f7094f
Gandhi Tech AI <kumarayshu852@gmail.com>
2026-04-28 12:57:53 +0530

    Update Home.js
    add new project

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -112,6 +112,10 @@ function Home() {
                      <Link to="https://genre-walnut-59802026.figma.site/">
                     <button className="ko">15.English Learning Roadmap</button>
                 </Link>
+                    </Link>
+                     <Link to="https://auraverse-ai.vercel.app/">
+                    <button className="ko">16. AURAVERSE — Advanced AI Interface</button>
+                </Link>
         </div>
 
 
